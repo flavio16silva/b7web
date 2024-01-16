853db4ae6ee3cc431902a5a408b7604430a8a091
Flavio Silva <fal_silva@hotmail.com>
2024-01-15 22:41:07 -0300

    Luta.js - local das lutas

---
+++ estudo/projetoLuta/luta.js
@@ -0,0 +1,10 @@
+let personagem = new Guerreiro('Leite')
+
+console.log(personagem.nome)
+console.log(personagem.life)
+console.log(personagem.attack)
+
+let monstro = new pequenoMonstro()
+console.log(monstro.nome)
+console.log(monstro.attack)
+
