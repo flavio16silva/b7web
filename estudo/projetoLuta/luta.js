083d8aea5b14e603865eecb1aaf83fb9ceb66d78
Flavio Silva <fal_silva@hotmail.com>
2024-01-15 23:37:50 -0300

    execução das lutas

--- estudo/projetoLuta/luta.js
+++ estudo/projetoLuta/luta.js
@@ -1,10 +1,19 @@
-let personagem = new Guerreiro('Leite')
+let personagem = new Mago('Leite')
 
 console.log(personagem.nome)
 console.log(personagem.life)
 console.log(personagem.attack)
 
-let monstro = new pequenoMonstro()
-console.log(monstro.nome)
-console.log(monstro.attack)
+let monster = new PequenoMonstro()
+console.log(monster.nome)
+console.log(monster.attack)
 
+//Inicializando o jogo
+const cenario = new Cenario(
+  char,
+  monster,
+  document.querySelector('#char'),
+  document.querySelector('#monster')
+)
+
+cenario.inicio()
